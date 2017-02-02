b0d4de5ca9e4227cd79a8b6248bdaf8b47f3fb89
Jason Morrissette <j.morrissette@jamdevs.com>
2017-02-02 10:59:29 -0800

    FEAT: Renamed from blog to personal
    renamed root application to personal to better build out the blog
    application.

---
+++ mysite/personal/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+# Renders Home/Index Page
+def index(request):
+    return render(request, 'personal/home.html')
+
+
+def contact(request):
+    return render(request, 'personal/contact.html', {'content':['If you would like to contact me, please email me at',
+                                                            'J.morrissette@jamdevs.com']})
