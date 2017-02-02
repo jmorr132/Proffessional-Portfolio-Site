a7b103cb7f5338b804aa6631a73763b6070e2ace
Jason Morrissette <j.morrissette@jamdevs.com>
2017-02-02 09:21:10 -0800

    Feat: Added Contact Me page
    Added a Contact Me Page

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Renders Home/Index Page
 def index(request):
     return render(request, 'blog/home.html')
+
+
+def contact(request):
+    return render(request, 'blog/basic.html', {'content':['If you would like to contact me, please email me at','J.morrissette@jamdevs.com']})
